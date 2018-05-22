bdcdd508d0ffc4287da1a340754f4bb5a682d0dc
Sywrah <sywrahgabriella@hotmail.com>
2018-05-22 00:58:10 -0300

    simplificando as views

--- Topicos Especiais em Programação/gamesapi_2/games/urls.py
+++ Topicos Especiais em Programação/gamesapi_2/games/urls.py
@@ -5,17 +5,14 @@ Author: Gaston C. Hillar - Twitter.com/gastonhillar
 Publisher: Packt Publishing Ltd. - http://www.packtpub.com
 """
 from django.urls import path
-from games import views
-from games.models import Game
-from games.serializers import GameSerializer
+from games.views import Games, GameCategories, Scores, Players
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('games/', views.GameList.as_view(), name=views.GameList.name),
-    path('games/<int:pk>/', views.GameDetail.as_view(), name=views.GameDetail.name),
-    path('game-categories/', views.GameCategoryList.as_view(), name=views.GameCategoryList.name),
-    path('game-categories/<int:pk>/', views.GameCategoryDetail.as_view(), name=views.GameCategoryDetail.name),
-    path('players/', views.PlayerList.as_view(), name=views.PlayerList.name),
-	path('players/<int:pk>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
-	path('scores/', views.ScoreList.as_view(), name=views.ScoreList.name),
-	path('scores/<int:pk>/', views.ScoreDetail.as_view(), name=views.ScoreDetail.name),
-	]
+router = DefaultRouter()
+router.register(r'games', Games)
+router.register(r'game-categories', GameCategories)
+router.register(r'players', Players)
+router.register(r'scores', Scores)
+
+urlpatterns = router.urls
+   
--- Topicos Especiais em Programação/gamesapi_2/games/views.py
+++ Topicos Especiais em Programação/gamesapi_2/games/views.py
@@ -1,51 +1,24 @@
-from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Game, GameCategory, Score, Player
 from .serializers import GameSerializer, GameCategorySerializer, PlayerSerializer, ScoreSerializer
 
-class GameCategoryList(generics.ListCreateAPIView):
+class GameCategories(ModelViewSet):
 	queryset = GameCategory.objects.all()
 	serializer_class = GameCategorySerializer
-	name = 'gamecategory-list'
+	
 
-
-class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+class Games(ModelViewSet):
 	queryset = GameCategory.objects.all()
 	serializer_class = GameCategorySerializer
-	name = 'gamecategory-detail'
 
 
-class GameList(generics.ListCreateAPIView):
-	queryset = Game.objects.all()
-	serializer_class = GameSerializer
-	name = 'game-list'
-
-
-class GameDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Game.objects.all()
-	serializer_class = GameSerializer
-	name = 'game-detail' 
-
-
-class PlayerList(generics.ListCreateAPIView):
+class Players(ModelViewSet):
 	queryset = Player.objects.all()
 	serializer_class = PlayerSerializer
-	name = 'player-list'
 
 
-class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Player.objects.all()
-	serializer_class = PlayerSerializer
-	name = 'player-detail'
-
-
-class ScoreList(generics.ListCreateAPIView):
+class Scores(ModelViewSet):
 	queryset = Score.objects.all()
 	serializer_class = ScoreSerializer
-	name = 'score-list'
-
-
-class ScoreDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Score.objects.all()
-	serializer_class = ScoreSerializer
-	name = 'score-detail'
+	
 
